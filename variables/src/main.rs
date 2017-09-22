0a621a2a785765d1a663f6beaa958010d092e5b7
Cole Lawrence <colelawrence@mobilgov.com>
2017-09-22 12:45:56 -0500

    on to branches

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,33 @@
+macro_rules! trace {
+    ($e:expr) => {
+        println!("TRACE ({}:{}) => {:?}", file!(), line!(), $e);
+    }
+}
+
+fn greet(name: &str) {
+    println!("Hello, {}!", name)
+}
+
+fn len(name: &str) -> i64 {
+    // notice importance of no semicolon here:
+    // otherwise the type would be ()
+    3
+}
+
 fn main() {
-    println!("Hello, world!");
+    greet("Carlos");
+    // let statement : identifier assigned to expression
+    let guess: usize = "42".parse().expect("number");
+    // semicolons denote the end of a statement
+    let x = 1;
+    let y = {
+        let x = x + 2;
+        x + 1
+    };
+    /*
+        hello
+    */
+    trace!(x);
+    trace!(y);
+    trace!(len("hey"));
 }
